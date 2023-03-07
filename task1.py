6395e971fd265e9377537e2e4f0f5c75fce4dc8e
Ali Thursland <alexandra.thursland@duke.edu>
2023-03-07 17:31:42 -0500

    finishing density function

--- task1.py
+++ task1.py
@@ -10,104 +10,76 @@ import sys
 import statistics
 
 ### define global variables
-filename = 'geolife-cars.csv'
-raw_data = []
-
-class Node: 
-    def __init__(self, id, x, y):
-        self.id = id # unique id
-        self.x = x # x-coord
-        self.y = y # y-coord 
-        self.size = 1 # size of subtrees below
-        self.left = None
-        self.right = None
-
-class BST:
-    """
-    BST class, where insert is sorted by 
-    """
-    def __init__(self):
-        self.root = None
-
-    ### ALL THE X METHODS: search, insert
-
-    def x_search(self, node):
-        """
-        search for a given point based on id field
-        """
-        if self.root is None or self.root.id == node.id: # base case
-            return self.root
-        
-        # node has a greater x value than root
-        if self.root.x < node.x:
-            return self.search(self.root.right, node)
-        
-        # node has a smaller or equal x value than root
-        return self.search(self.root.left, node)
-        
-    def x_add(self, point):
-        node = Node(point[0], point[1], point[2])
-        self.x_insert(self.root, node)
-    
-    def x_insert(self, root, node):
-        if root == None:
-            self.root = node
-        else:
-            root.size += 1 
-            if root.x > node.x:
-                if root.left == None:
-                    root.left = node
-                else:
-                    self.x_insert(root.left, node)
-            else: # is root.x == node.x we insert node into the right subtree
-                if root.right == None:
-                    root.right = node
-                else:
-                    self.x_insert(root.right, node)
-
-    ### ALL THE Y METHODS: search, insert
-
-    def y_add(self, point):
-        """
-        insert row into BST based on y-value of p 
-        """
-        node = Node(point[0], point[1], point[2])
-        self.y_insert(self.root, node)
-    
-    def y_insert(self, root, node):
-        if root == None:
-            self.root = node
-        else:
-            root.size += 1 
-            if root.y > node.y:
-                if root.left == None:
-                    root.left = node
-                else:
-                    self.y_insert(root.left, node)
-            else: # if root.y == node.y we insert node into the right subtree
-                if root.right == None:
-                    root.right = node
-                else:
-                    self.y_insert(root.right, node)
+filename = 'geolife-cars-ten-percent.csv'
+data = []
+r = 1
+grid = None
+DENOM = 20
 
 def import_data(fname):
     """
     import data from csv
     """
+    global data
+
     with open(fname, newline='', encoding='utf-8') as f:
         reader = csv.reader(f)
         next(reader, None) # skip the headers
         for row in reader:
-            id = row[1]
-            print(row[2])
-            print(row[3])
             x = float(row[2])
             y = float(row[3])
-            raw_data.append((id, x, y))
+            data.append((x, y))
 
-def density(point, r):
+def get_cell_indices(point):
+    """
+    helper function for preprocess - 
+    given a point p, decide which cell it belongs in
+    """
+    minx = min(point[0] for point in data)
+    miny = min(point[1] for point in data)
+    col = int(point[0]-minx / r)
+    row = int(point[1]-miny / r)
+    return col, row
+
+def preprocess(data):
+    """
+    using the input data, find the optimal value of r
+    to be used for pre-processing (creating the grid)
+    O (n log n) time for sorting
+    """
+    # define width and height of our plane (based on input)
+    width = abs(max(point[0] for point in data) - min(point[0] for point in data))
+    height = abs(max(point[1] for point in data) - min(point[0] for point in data))
+
+    # define number of columns and rows for our grid 
+    num_cols = int(width / r)
+    num_rows = int(height / r)
+
+    # initialize an empty grid with the appropriate number of cells
+    global grid 
+    grid = [[[] for _ in range(num_cols)] for _ in range(num_rows)]
+
+    for point in data: 
+        col, row = get_cell_indices(point)
+        grid[row][col].append(point)
+
+    for row in grid:
+        for cell in row:
+            print("flag2")
+            cell.sort()
+
+def density(p):
     """
     find the density for a given point
+    """
+    cell = get_cell_indices(p)
+
+    return len(cell)
+
+def hubs(k, r):
+    """
+    identify k > - hubs of high density s.t. 
+    any two hubs re separated by at least distance r
     """
     return
 
@@ -115,19 +87,13 @@ def main():
     """
     main function
     1. import the data from csv file 
-    2. create and populate x_bst - during insertion, update density
-    3. create and populate y_bst
-    4. for a given r, calculate 
+    2. pre-process the data
+    3. call density
+    4. call hubs
     """
-    import_data(filename)
-    x_bst = BST()
-    y_bst = BST()
-
-    for row in raw_data:
-        x_bst.x_add(row)
-        y_bst.y_add(row)
-
-    return(x_bst.root.id, y_bst.root.id)
+    import_data(filename) # step 1
+    print(grid)
+    return preprocess(data) # step 2
 
 if __name__ == "__main__":
     print(main())
